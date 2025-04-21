deb3d439e1794c76809a574095900d4f6fa3c13f
miles-franklin-wright <miles.f.w.7@gmail.com>
2025-04-20 21:41:04 -0400

    finally have a working heartbeat - remaining steps for this are going to be hook up through dbt to postgres to superset for E2E success. Then digital ocean hosting to ensure I can push it out to "prod". Then add in the business logic.

--- bytewax/flows/heartbeat_flow.py
+++ bytewax/flows/heartbeat_flow.py
@@ -8,7 +8,7 @@ from bytewax.connectors.kafka import KafkaSource
 from bytewax.connectors.files import FileSink
 
 
-if __name__ == "__main__":
+def build_flow() -> Dataflow:
     # Read env vars with sensible defaults
     broker = os.getenv("REDPANDA_BROKER", "redpanda:9092")
     topic = os.getenv("HEARTBEAT_TOPIC", "heartbeat")
@@ -18,13 +18,14 @@ if __name__ == "__main__":
     flow = Dataflow("heartbeat_flow")
 
     # Step 1: Read from Kafka
-    inp = op.input("in", flow, KafkaSource([topic], broker))
+    inp = op.input("in", flow, KafkaSource(topics=[topic], brokers=[broker]))
 
     # Step 2: Parse JSON
-    prse = op.map("parse", inp, lambda msg: json.loads(msg.value))
+    kv_msgs = op.map(
+        "to_kv", inp, lambda msg: ("heartbeat", json.dumps(json.loads(msg.value)))
+    )
 
     # Step 3: Write to file
-    op.output("out", prse, FileSink(bronze_path))
+    op.output("out", kv_msgs, FileSink(bronze_path))
 
-    # Step 4: Run the flow
-    flow.run()
+    return flow
